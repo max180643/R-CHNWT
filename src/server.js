7bf883262765b3a988bfdcb993581746f2140285
max180643 <max180643@gmail.com>
2020-08-20 03:35:56 +0700

    :heavy_plus_sign: add cors dependency

--- src/server.js
+++ src/server.js
@@ -4,11 +4,15 @@ const express = require('express')
 
 const bodyParser = require('body-parser')
 
+const cors = require('cors')
+
 const app = express()
 
 const port = process.env.PORT || 5555
 
 app.use(bodyParser.json())
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send({
